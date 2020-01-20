9d57f24cd8378f5f52a2c0c5949c556c3ad68c1e
KIMINJOONG <ijboym216@gmail.com>
2020-01-20 23:51:32 +0900

    http protocol

---
+++ 09.크롤링/sock.py
@@ -0,0 +1,16 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+google_ip = socket.gethostbyname("google.com")
+
+sock.connect((google_ip, 80))
+
+sock.send("GET / HTTP/1.1\n".encode())
+sock.send("\n".encode())
+
+buffer = sock.recv(4096)
+buffer.decode().replace("\r\n", "\n")
+sock.close()
+
+print(buffer)
